d1a4dd43c3e09a98f44ffb873d2a033bec229788
TychoCRD <rtcotton@gmail.com>
2017-09-08 15:37:35 +0800

    Add modules folder and bootstrap css, create searchbox component

---
+++ src/modules/SearchBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="searchbox-container">
+    <div class="search-form">
+      <input type="text" v-model="keyword" :placeholder="placeholder"/>
+      <button @click.prevent="startSearch"></button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SearchBox',
+  data () {
+    return {
+      keyword: '',
+      placeholder: 'Tell me what image you want!'
+    }
+  },
+  methods: {
+    startSearch () {
+      if (!this.keyword) return
+      alert(keyword)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .searchbox-container {
+    display: flex;
+    justify-content: center;
+  }
+</style>
